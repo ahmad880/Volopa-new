9877f3e33f9914ec1abbf578dd7c175332d24353
Ahmad <aahhmmaadd.bkr@gmail.com>
2025-02-27 17:31:14 +0500

    new fixes to add recipient

--- cypress/e2e/massRecipient.cy.js
+++ cypress/e2e/massRecipient.cy.js
@@ -19,168 +19,135 @@ describe('Mass Recipients ',function(){
         cy.visit('https://webapp6.volopa.com/', { timeout: 10000 })
         cy.viewport(1440,1000)
     })
-    it.only('TC-Mass recipient for adding business - ', function(){
-            signin.Login(userName, password);
+    it('TC- Mass Payments - Add mass recipients', function(){
+        // Step 1: Login once
+        signin.Login(userName, password);
     
-            Cypress._.times(50, () => {  
-                cy.log('Iteration started');
+        // Step 2: Repeat the process after logging in
+        Cypress._.times(30, (index) => {  
+            cy.log(`Iteration ${index + 1} started`);
     
-                const randomBusinessName = `Business ${faker.company.name()}`;
+            const randomName = `QA ${faker.person.firstName()} ${faker.person.lastName()}`;
     
-                newRecipient.goToPaymentsDashborad();
-                newRecipient.gotoRecipientList();
-                newRecipient.addRecipient('UNITED KINGDOM{enter}', 'GBP{enter}');
-                newRecipient.addBankDetails('HU42117730161111101800000000', 'AKKHHUHB');
-                newRecipient.addBusinessRecipientforMassTesting('United Kingdom{enter}', randomBusinessName);
-                newRecipient.saveRecipient();
-                newRecipient.checkSettelment('be.disabled', 'be.enabled');
+            newRecipient.goToPaymentsDashborad();
+            newRecipient.gotoRecipientList();
+            newRecipient.addRecipient('United Kingdom{enter}', 'GBP{enter}');
+            newRecipient.addBankDetails('GB14BARC20038054413881', 'BUKBGB22');
+            newRecipient.individualRecipient(randomName, 'United Kingdom{enter}');
+            newRecipient.saveRecipient();
+            newRecipient.checkSettelment('be.disabled', 'be.enabled');
     
-                cy.log(`Iteration completed with Business Name: ${randomBusinessName}`);
-            });
+            cy.log(`Iteration ${index + 1} completed with name: ${randomName}`);
+            console.log(`Iteration ${index + 1} completed with name: ${randomName}`);
         });
-
+    });
+    
+    it('TC-Mass recipient for adding business - ', function(){
+        signin.Login(userName, password);
+    
+        Cypress._.times(30, (index) => {  
+            cy.log(`Iteration ${index + 1} started`);
+    
+            const randomBusinessName = `Business ${faker.company.name()}`;
+    
+            newRecipient.goToPaymentsDashborad();
+            newRecipient.gotoRecipientList();
+            newRecipient.addRecipient('UNITED KINGDOM{enter}', 'GBP{enter}');
+            newRecipient.addBankDetails('HU42117730161111101800000000', 'AKKHHUHB');
+            newRecipient.addBusinessRecipientforMassTesting('United Kingdom{enter}', randomBusinessName);
+            newRecipient.saveRecipient();
+            newRecipient.checkSettelment('be.disabled', 'be.enabled');
+    
+            console.log(`Iteration ${index + 1} completed with Business Name: ${randomBusinessName}`);
+        });
+    });
+    
+    it('TC- Mass Payments - Add mass recipients', function(){
+        // Step 1: Login once
+        signin.Login(userName, password);
+    
+        // Step 2: Repeat the process after logging in
+        Cypress._.times(30, (index) => {  
+            cy.log(`Iteration ${index + 1} started`);
+    
+            const randomName = `QA ${faker.person.firstName()} ${faker.person.lastName()}`;
+    
+            newRecipient.goToPaymentsDashborad();
+            newRecipient.gotoRecipientList();
+            newRecipient.addRecipient('United Kingdom{enter}', 'GBP{enter}');
+            newRecipient.addBankDetails('GB14BARC20038054413881', 'BUKBGB22');
+            newRecipient.individualRecipient(randomName, 'United Kingdom{enter}');
+            newRecipient.saveRecipient();
+            newRecipient.checkSettelment('be.disabled', 'be.enabled');
+    
+            cy.log(`Iteration ${index + 1} completed with name: ${randomName}`);
+            console.log(`Iteration ${index + 1} completed with name: ${randomName}`);
+        });
+    });
+    
     // Code for business recipient with currency GBP
-        it.only('TC-Mass recipient for adding business - ', function(){
-            signin.Login(userName, password);
-    
-            Cypress._.times(50, () => {  
-                cy.log('Iteration started');
-    
-                const randomBusinessName = `Business ${faker.company.name()}`;
-    
-                newRecipient.goToPaymentsDashborad();
-                newRecipient.gotoRecipientList();
-                newRecipient.addRecipient('UNITED KINGDOM{enter}', 'GBP{enter}');
-                newRecipient.addBankDetails('HU42117730161111101800000000', 'AKKHHUHB');
-                newRecipient.addBusinessRecipientforMassTesting('United Kingdom{enter}', randomBusinessName);
-                newRecipient.saveRecipient();
-                newRecipient.checkSettelment('be.disabled', 'be.enabled');
-    
-                cy.log(`Iteration completed with Business Name: ${randomBusinessName}`);
-            });
-        });  
+    it('TC-Mass recipient for adding business - ', function(){
+    signin.Login(userName, password);
 
-        it.only('TC-Mass recipient for adding business - ', function(){
-            signin.Login(userName, password);
-    
-            Cypress._.times(50, () => {  
-                cy.log('Iteration started');
-    
-                const randomBusinessName = `Business ${faker.company.name()}`;
-    
-                newRecipient.goToPaymentsDashborad();
-                newRecipient.gotoRecipientList();
-                newRecipient.addRecipient('UNITED KINGDOM{enter}', 'GBP{enter}');
-                newRecipient.addBankDetails('HU42117730161111101800000000', 'AKKHHUHB');
-                newRecipient.addBusinessRecipientforMassTesting('United Kingdom{enter}', randomBusinessName);
-                newRecipient.saveRecipient();
-                newRecipient.checkSettelment('be.disabled', 'be.enabled');
-    
-                cy.log(`Iteration completed with Business Name: ${randomBusinessName}`);
-            });
-        });
+    Cypress._.times(30, (index) => {  
+        cy.log(`Iteration ${index + 1} started`);
 
-    // Code for business recipient with currency GBP
-        it.only('TC-Mass recipient for adding business - ', function(){
-            signin.Login(userName, password);
-    
-            Cypress._.times(50, () => {  
-                cy.log('Iteration started');
-    
-                const randomBusinessName = `Business ${faker.company.name()}`;
-    
-                newRecipient.goToPaymentsDashborad();
-                newRecipient.gotoRecipientList();
-                newRecipient.addRecipient('UNITED KINGDOM{enter}', 'GBP{enter}');
-                newRecipient.addBankDetails('HU42117730161111101800000000', 'AKKHHUHB');
-                newRecipient.addBusinessRecipientforMassTesting('United Kingdom{enter}', randomBusinessName);
-                newRecipient.saveRecipient();
-                newRecipient.checkSettelment('be.disabled', 'be.enabled');
-    
-                cy.log(`Iteration completed with Business Name: ${randomBusinessName}`);
-            });
-        });
-        it.only('TC-Mass recipient for adding business - ', function(){
-            signin.Login(userName, password);
-    
-            Cypress._.times(50, () => {  
-                cy.log('Iteration started');
-    
-                const randomBusinessName = `Business ${faker.company.name()}`;
-    
-                newRecipient.goToPaymentsDashborad();
-                newRecipient.gotoRecipientList();
-                newRecipient.addRecipient('UNITED KINGDOM{enter}', 'GBP{enter}');
-                newRecipient.addBankDetails('HU42117730161111101800000000', 'AKKHHUHB');
-                newRecipient.addBusinessRecipientforMassTesting('United Kingdom{enter}', randomBusinessName);
-                newRecipient.saveRecipient();
-                newRecipient.checkSettelment('be.disabled', 'be.enabled');
-    
-                cy.log(`Iteration completed with Business Name: ${randomBusinessName}`);
-            });
-        });
+        const randomBusinessName = `Business ${faker.company.name()}`;
 
-    // Code for business recipient with currency GBP
-        it.only('TC-Mass recipient for adding business - ', function(){
-            signin.Login(userName, password);
-    
-            Cypress._.times(50, () => {  
-                cy.log('Iteration started');
-    
-                const randomBusinessName = `Business ${faker.company.name()}`;
-    
-                newRecipient.goToPaymentsDashborad();
-                newRecipient.gotoRecipientList();
-                newRecipient.addRecipient('UNITED KINGDOM{enter}', 'GBP{enter}');
-                newRecipient.addBankDetails('HU42117730161111101800000000', 'AKKHHUHB');
-                newRecipient.addBusinessRecipientforMassTesting('United Kingdom{enter}', randomBusinessName);
-                newRecipient.saveRecipient();
-                newRecipient.checkSettelment('be.disabled', 'be.enabled');
-    
-                cy.log(`Iteration completed with Business Name: ${randomBusinessName}`);
-            });
-        });
-        it.only('TC-Mass recipient for adding business - ', function(){
-            signin.Login(userName, password);
-    
-            Cypress._.times(50, () => {  
-                cy.log('Iteration started');
-    
-                const randomBusinessName = `Business ${faker.company.name()}`;
-    
-                newRecipient.goToPaymentsDashborad();
-                newRecipient.gotoRecipientList();
-                newRecipient.addRecipient('UNITED KINGDOM{enter}', 'GBP{enter}');
-                newRecipient.addBankDetails('HU42117730161111101800000000', 'AKKHHUHB');
-                newRecipient.addBusinessRecipientforMassTesting('United Kingdom{enter}', randomBusinessName);
-                newRecipient.saveRecipient();
-                newRecipient.checkSettelment('be.disabled', 'be.enabled');
-    
-                cy.log(`Iteration completed with Business Name: ${randomBusinessName}`);
-            });
-        });
+        newRecipient.goToPaymentsDashborad();
+        newRecipient.gotoRecipientList();
+        newRecipient.addRecipient('UNITED KINGDOM{enter}', 'GBP{enter}');
+        newRecipient.addBankDetails('HU42117730161111101800000000', 'AKKHHUHB');
+        newRecipient.addBusinessRecipientforMassTesting('United Kingdom{enter}', randomBusinessName);
+        newRecipient.saveRecipient();
+        newRecipient.checkSettelment('be.disabled', 'be.enabled');
 
-    // Code for business recipient with currency GBP
-        it.only('TC-Mass recipient for adding business - ', function(){
-            signin.Login(userName, password);
-    
-            Cypress._.times(50, () => {  
-                cy.log('Iteration started');
-    
-                const randomBusinessName = `Business ${faker.company.name()}`;
-    
-                newRecipient.goToPaymentsDashborad();
-                newRecipient.gotoRecipientList();
-                newRecipient.addRecipient('UNITED KINGDOM{enter}', 'GBP{enter}');
-                newRecipient.addBankDetails('HU42117730161111101800000000', 'AKKHHUHB');
-                newRecipient.addBusinessRecipientforMassTesting('United Kingdom{enter}', randomBusinessName);
-                newRecipient.saveRecipient();
-                newRecipient.checkSettelment('be.disabled', 'be.enabled');
-    
-                cy.log(`Iteration completed with Business Name: ${randomBusinessName}`);
-            });
-        });   
-    
+        cy.log(`Iteration ${index + 1} completed with Business Name: ${randomBusinessName}`);
+        console.log(`Iteration ${index + 1} completed with Business Name: ${randomBusinessName}`);
+    });
+    });
 
+    it('TC- Mass Payments - Add mass recipients', function(){
+    // Step 1: Login once
+    signin.Login(userName, password);
 
+    // Step 2: Repeat the process after logging in
+    Cypress._.times(30, (index) => {  
+        cy.log(`Iteration ${index + 1} started`);
+
+        const randomName = `QA ${faker.person.firstName()} ${faker.person.lastName()}`;
+
+        newRecipient.goToPaymentsDashborad();
+        newRecipient.gotoRecipientList();
+        newRecipient.addRecipient('United Kingdom{enter}', 'GBP{enter}');
+        newRecipient.addBankDetails('GB14BARC20038054413881', 'BUKBGB22');
+        newRecipient.individualRecipient(randomName, 'United Kingdom{enter}');
+        newRecipient.saveRecipient();
+        newRecipient.checkSettelment('be.disabled', 'be.enabled');
+
+        cy.log(`Iteration ${index + 1} completed with name: ${randomName}`);
+        console.log(`Iteration ${index + 1} completed with name: ${randomName}`);
+    });
+    });
+
+    it('TC-Mass recipient for adding business - ', function(){
+    signin.Login(userName, password);
+
+    Cypress._.times(30, (index) => {  
+        cy.log(`Iteration ${index + 1} started`);
+
+        const randomBusinessName = `Business ${faker.company.name()}`;
+
+        newRecipient.goToPaymentsDashborad();
+        newRecipient.gotoRecipientList();
+        newRecipient.addRecipient('UNITED KINGDOM{enter}', 'GBP{enter}');
+        newRecipient.addBankDetails('HU42117730161111101800000000', 'AKKHHUHB');
+        newRecipient.addBusinessRecipientforMassTesting('United Kingdom{enter}', randomBusinessName);
+        newRecipient.saveRecipient();
+        newRecipient.checkSettelment('be.disabled', 'be.enabled');
+
+        console.log(`Iteration ${index + 1} completed with Business Name: ${randomBusinessName}`);
+    });
+    });
+        
 })

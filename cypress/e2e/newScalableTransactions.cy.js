fa5cc34165b2497e03539464ad9d0669fc4be64f
QA <talha.naeem@volopa.com>
2025-10-03 18:11:13 +0500

    Card Load

--- cypress/e2e/newScalableTransactions.cy.js
+++ cypress/e2e/newScalableTransactions.cy.js
@@ -28,7 +28,7 @@ describe("Compare Transaction History Table - App1 vs App2", () => {
 
     //Apply transaction type filter
     cy.get('th:nth-child(3) div:nth-child(1) span:nth-child(2)').click();
-    cy.get('label.ant-radio-wrapper span').contains('Credit (Payment Transaction)').click();
+    cy.get('label.ant-radio-wrapper span').contains('Card Load').click();
     cy.get(".ant-spin-dot").should("not.exist");
     cy.wait(5000);
 
@@ -51,7 +51,7 @@ describe("Compare Transaction History Table - App1 vs App2", () => {
             cy.wait(5000);
             //Apply transaction type filter
             cy.get('th:nth-child(3) div:nth-child(1) span:nth-child(2)').click();
-            cy.get('label.ant-radio-wrapper span').contains('Credit (Payment Transaction)').click();
+            cy.get('label.ant-radio-wrapper span').contains('Card Load').click();
             cy.get(".ant-spin-dot").should("not.exist");
             cy.wait(5000);
 
